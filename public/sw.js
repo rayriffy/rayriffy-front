dad147a0aed2b6a3baa1c99b3d4501c9f2165d51
Phumrapee Limpianchop <git@rayriffy.com>
2018-10-21 16:30:59 +0700

    :tada: Intitial C

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+workbox.routing.registerRoute(/\.(?:html)$/,
+  workbox.strategies.staleWhileRevalidate()
+);
+
+self.addEventListener('install', event => {
+  const urls = [
+    'https://cdn.ampproject.org/v0/amp-analytics-0.1.js',
+    'https://cdn.ampproject.org/v0/amp-bind-0.1.js',
+    'https://cdn.ampproject.org/v0/amp-carousel-0.1.js',
+    'https://cdn.ampproject.org/v0/amp-font-0.1.js',
+    'https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.js',
+    'https://cdn.ampproject.org/v0/amp-list-0.1.js',
+    'https://cdn.ampproject.org/v0/amp-mustache-0.2.js',
+    'https://use.fontawesome.com/releases/v5.4.1/css/all.css',
+    'https://fonts.googleapis.com/css?family=Open+Sans',
+    'index.html',
+    '/'
+  ];
+  const cacheName = workbox.core.cacheNames.runtime;
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(urls))
+  );
+});
